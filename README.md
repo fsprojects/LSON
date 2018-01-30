d8ee202678a8d86a4399f50ad171cf8f81a74f2d
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2018-01-30 19:19:56 +0100

    Setup of project  (#1)
    * First take
    
    * Adding more text

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # LSON
-Lisp inspired serialization
+
+Lisp inspired serialization. Lisp has a wonderfully simple syntax. This can be used to [write code](https://www.amazon.com/Little-LISPer-Third-Daniel-Friedman/dp/0023397632) or potentially a simple alternative when a simple format is needed.
+
+MacOS/Linux | Windows
+--- | ---
+[![Travis Badge](https://travis-ci.org/fsprojects/LSON.svg?branch=master)](https://travis-ci.org/fsprojects/LSON) | [![Build status](https://ci.appveyor.com/api/projects/status/github/fsprojects/LSON?svg=true)](https://ci.appveyor.com/project/fsprojects/LSON)
+[![Build History](https://buildstats.info/travisci/chart/fsprojects/LSON)](https://travis-ci.org/fsprojects/LSON/builds) | [![Build History](https://buildstats.info/appveyor/chart/fsprojects/LSON)](https://ci.appveyor.com/project/fsprojects/LSON)  
+
+
+## Nuget 
+
+Stable | Prerelease
+--- | ---
+[![NuGet Badge](https://buildstats.info/nuget/LSON)](https://www.nuget.org/packages/LSON/) | [![NuGet Badge](https://buildstats.info/nuget/LSON?includePreReleases=true)](https://www.nuget.org/packages/LSON/)
+
+
+
+Does the thing
---
+++ build.fsx
@@ -0,0 +1,181 @@
+#r @"packages/build/FAKE/tools/FakeLib.dll"
+open Fake
+open Fake.Git
+open Fake.AssemblyInfoFile
+open Fake.ReleaseNotesHelper
+open Fake.UserInputHelper
+open System
+
+let release = LoadReleaseNotes "RELEASE_NOTES.md"
+let srcGlob = "src/**/*.fsproj"
+let testsGlob = "tests/**/*.fsproj"
+
+Target "Clean" (fun _ ->
+    ["bin"; "temp" ;"dist"]
+    |> CleanDirs
+
+    !! srcGlob
+    ++ testsGlob
+    |> Seq.collect(fun p ->
+        ["bin";"obj"]
+        |> Seq.map(fun sp ->
+             IO.Path.GetDirectoryName p @@ sp)
+        )
+    |> CleanDirs
+
+    )
+
+Target "DotnetRestore" (fun _ ->
+    !! srcGlob
+    ++ testsGlob
+    |> Seq.iter (fun proj ->
+        DotNetCli.Restore (fun c ->
+            { c with
+                Project = proj
+                //This makes sure that Proj2 references the correct version of Proj1
+                AdditionalArgs = [sprintf "/p:PackageVersion=%s" release.NugetVersion]
+            })
+))
+
+Target "DotnetBuild" (fun _ ->
+    !! srcGlob
+    |> Seq.iter (fun proj ->
+        DotNetCli.Build (fun c ->
+            { c with
+                Project = proj
+                //This makes sure that Proj2 references the correct version of Proj1
+                AdditionalArgs = [sprintf "/p:PackageVersion=%s" release.NugetVersion]
+            })
+))
+
+let invoke f = f ()
+let invokeAsync f = async { f () }
+
+type TargetFramework =
+| Full of string
+| Core of string
+
+let (|StartsWith|_|) prefix (s: string) =
+    if s.StartsWith prefix then Some () else None
+
+let getTargetFramework tf =
+    match tf with
+    | StartsWith "net4" -> Full tf
+    | StartsWith "netcoreapp" -> Core tf
+    | _ -> failwithf "Unknown TargetFramework %s" tf
+
+let getTargetFrameworksFromProjectFile (projFile : string)=
+    let doc = Xml.XmlDocument()
+    doc.Load(projFile)
+    doc.GetElementsByTagName("TargetFrameworks").[0].InnerText.Split(';')
+    |> Seq.map getTargetFramework
+    |> Seq.toList
+
+let selectRunnerForFramework tf =
+    let runMono = sprintf "mono -f %s -c Release"
+    let runCore = sprintf "run -f %s -c Release"
+    match tf with
+    | Full t when isMono-> runMono t
+    | Full t -> runCore t
+    | Core t -> runCore t
+
+
+let runTests modifyArgs =
+    !! testsGlob
+    |> Seq.map(fun proj -> proj, getTargetFrameworksFromProjectFile proj)
+    |> Seq.collect(fun (proj, targetFrameworks) ->
+        targetFrameworks
+        |> Seq.map selectRunnerForFramework
+        |> Seq.map(fun args -> fun () ->
+            DotNetCli.RunCommand (fun c ->
+            { c with
+                WorkingDir = IO.Path.GetDirectoryName proj
+            }) (modifyArgs args))
+    )
+
+
+Target "DotnetTest" (fun _ ->
+    runTests id
+    |> Seq.iter (invoke)
+)
+let execProcAndReturnMessages filename args =
+    let args' = args |> String.concat " "
+    ProcessHelper.ExecProcessAndReturnMessages
+                (fun psi ->
+                    psi.FileName <- filename
+                    psi.Arguments <-args'
+                ) (TimeSpan.FromMinutes(1.))
+
+let pkill args =
+    execProcAndReturnMessages "pkill" args
+
+let killParentsAndChildren processId=
+    pkill [sprintf "-P %d" processId]
+
+
+Target "WatchTests" (fun _ ->
+    runTests (sprintf "watch %s")
+    |> Seq.iter (invokeAsync >> Async.Catch >> Async.Ignore >> Async.Start)
+
+    printfn "Press enter to stop..."
+    Console.ReadLine() |> ignore
+
+    if isWindows |> not then
+        startedProcesses
+        |> Seq.iter(fst >> killParentsAndChildren >> ignore )
+    else
+        //Hope windows handles this right?
+        ()
+)
+
+Target "DotnetPack" (fun _ ->
+    !! srcGlob
+    |> Seq.iter (fun proj ->
+        DotNetCli.Pack (fun c ->
+            { c with
+                Project = proj
+                Configuration = "Release"
+                OutputPath = IO.Directory.GetCurrentDirectory() @@ "dist"
+                AdditionalArgs =
+                    [
+                        sprintf "/p:PackageVersion=%s" release.NugetVersion
+                        sprintf "/p:PackageReleaseNotes=\"%s\"" (String.Join("\n",release.Notes))
+                        "/p:SourceLinkCreate=true"
+                    ]
+            })
+    )
+)
+
+Target "Publish" (fun _ ->
+    Paket.Push(fun c ->
+            { c with
+                PublishUrl = "https://www.nuget.org"
+                WorkingDir = "dist"
+            }
+        )
+)
+
+Target "Release" (fun _ ->
+
+    if Git.Information.getBranchName "" <> "master" then failwith "Not on master"
+
+    StageAll ""
+    Git.Commit.Commit "" (sprintf "Bump version to %s" release.NugetVersion)
+    Branches.push ""
+
+    Branches.tag "" release.NugetVersion
+    Branches.pushTag "" "origin" release.NugetVersion
+)
+
+"Clean"
+  ==> "DotnetRestore"
+  ==> "DotnetBuild"
+  ==> "DotnetTest"
+  ==> "DotnetPack"
+  ==> "Publish"
+  ==> "Release"
+
+"DotnetRestore"
+ ==> "WatchTests"
+
+RunTargetOrDefault "DotnetPack"
